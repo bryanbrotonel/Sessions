77d28d7f1ce3ebfe2c6c5f4b3c5e7214fa083a72
Danny <sheerflames@gmail.com>
2020-11-28 09:56:54 -0800

    Timer Code with Background Code implemented

--- app/src/main/java/info3245/sessions/Timer.java
+++ app/src/main/java/info3245/sessions/Timer.java
@@ -1,5 +1,6 @@
 package info3245.sessions;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -13,11 +14,12 @@ public class Timer extends Container {
     private  long startingTime;
     private  long focusTimeStart = 6000;
     private  long breakTimeStart = 5000;
-    private TextView mTextViewCountDown, mTimerType;
-    private Button mPlay,mReset, mRestart;
+    private TextView mTextViewCountDown;
+    private Button mPlay,mReset, mFocus, mBreak;
     private CountDownTimer countDownTimer;
     private boolean mTimerRunning;
     private long timeRemaining;
+    private long mCurrentTime;
 
 
 //Timer Code
@@ -29,13 +31,15 @@ public class Timer extends Container {
         super.onCreate(savedInstanceState);
 
         //Timer Code
+
+        //initializing values
         startingTime = focusTimeStart;
         timeRemaining = startingTime;
         mTextViewCountDown = findViewById(R.id.timer);
-        mTimerType = findViewById(R.id.timerType);
         mPlay = findViewById(R.id.playButton);
         mReset =findViewById(R.id.resetButton);
-        mRestart = findViewById(R.id.restartButton);
+        mFocus = findViewById(R.id.focusButton);
+        mBreak = findViewById(R.id.breakButton);
 
         // When play button is press, Timer clicks down or pause
         mPlay.setOnClickListener(new View.OnClickListener() {
@@ -53,6 +57,7 @@ public class Timer extends Container {
             }
         });
 
+
         // When reset button is clicked, timer reset back to Starting Time
         mReset.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,26 +67,36 @@ public class Timer extends Container {
 
         });
 
-        // When Restart Button is clicked,  timer reset back to Starting Time and Timer Type is back to Focus
-        mRestart.setOnClickListener(new View.OnClickListener() {
+        // When Focus Button is clicked,  timer reset and will change the Starting Time to what was set in Focus
+        mFocus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mTimerType.getText().toString().equals("Break") )
-                {
-                    mTimerType.setText("Focus");
-                }
+
 
                 startingTime = focusTimeStart;
                 resetTimer();
 
             }
         });
-        updateCountDownText();
+
+        // When Focus Button is clicked,  timer reset and will change the Starting Time to what was set in Break
+        mBreak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                startingTime = breakTimeStart;
+                resetTimer();
+
+            }
+        });
 //Timer Code
 
 
 
     }
+
+    // Navigation Methods
 
     // Returns layout ID to Container Class
     @Override
@@ -101,8 +116,13 @@ public class Timer extends Container {
     // Countdown timer ticking down
     private void startTimer()
     {
+        //used for background calculation
+        mCurrentTime = System.currentTimeMillis() + timeRemaining;
+
+        //Countdown Class is used to do the timer countdown
         countDownTimer = new CountDownTimer(timeRemaining,1000) {
             @Override
+            //Timer will tickdown and will update the textview
             public void onTick(long l) {
 
                 timeRemaining = l;
@@ -111,24 +131,8 @@ public class Timer extends Container {
             }
 
             @Override
+            //When Timer reaches Zero
             public void onFinish() {
-
-
-                // Determine which timer type and setting the Starting time when finished
-                if(mTimerType.getText().toString().equals("Focus"))
-                {
-                    mTimerType.setText("Break");
-                    startingTime = breakTimeStart;
-                }
-
-                else
-                {
-                    mTimerType.setText("Focus");
-                    startingTime = focusTimeStart;
-                }
-                timeRemaining = startingTime;
-                updateCountDownText();
-
 
                 mTimerRunning = false;
                 mPlay.setText("Start");
@@ -151,7 +155,9 @@ public class Timer extends Container {
     // Reset The Timer back to starting time
     private void resetTimer()
     {
-        pauseTimer();
+        if(mTimerRunning==true)
+            pauseTimer();
+
         timeRemaining = startingTime;
         updateCountDownText();
 
@@ -169,6 +175,65 @@ public class Timer extends Container {
 
     }
 
-//Timer Code*/
 
+
+    // Background running Coding
+
+    // Upon switching activities or exiting the app, the app will store these data and stop the timer
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putLong("millisLeft", timeRemaining);
+        editor.putBoolean("timerRunning", mTimerRunning);
+        editor.putLong("endTime", mCurrentTime);
+        editor.putLong("startingTime", startingTime);
+        editor.apply();
+
+        // if countdown is still active, stop it
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+    }
+
+    //Upon switching back to the timer activity, a calculation is made to the timer to get the current time.
+    @Override
+    protected void onStart() {
+        super.onStart();
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        timeRemaining = prefs.getLong("millisLeft", startingTime);
+        mTimerRunning = prefs.getBoolean("timerRunning", false);
+        startingTime = prefs.getLong("startingTime", startingTime);
+        updateCountDownText();
+
+
+        if (mTimerRunning) {
+            mCurrentTime = prefs.getLong("endTime", 0);
+
+            //calc to get the current time
+            timeRemaining = mCurrentTime- System.currentTimeMillis();
+
+            //When the calculation reaches < 0, timer will make itself 0
+            if (timeRemaining < 0) {
+                timeRemaining = 0;
+                mTimerRunning = false;
+                updateCountDownText();
+                mPlay.setText("Start");
+
+            } else {
+                startTimer();
+            }
+
+
+        }
+        // When reset button is clicked, timer reset back to Starting Time
+        mReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetTimer();
+            }
+
+        });
+    }
 }
